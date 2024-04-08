0340e591b21adfe29091a139859ae0b94a65b13a
Axmdstar <85846658+Axmdstar@users.noreply.github.com>
2024-04-08 11:48:33 +0300

    Added React Router dom and tailwindcss

---
+++ Frontend/Nadiifiye/src/Pages/Layout/HomeLayout.jsx
@@ -0,0 +1,13 @@
+import {Outlet} from 'react-router-dom';
+
+const HomeLayout = () => {
+    return ( 
+        <>
+            <p>NavBar</p>
+                <Outlet />
+            <p>Footer</p>
+        </>
+     );
+}
+ 
+export default HomeLayout;
